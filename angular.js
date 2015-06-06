e01c00340584c645f440d7614f6cad5f18e96e2d
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:30:32 -0700

    researching how to properly salt + hash, but skipping for now

--- angular.js
+++ angular.js
@@ -3,7 +3,7 @@
 angular.module('login1', [])
   .controller('LoginController', function() {
     this.authenticate = function authenticate() {
-        var passhash = CryptoJS.MD5(this.password);
+        var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
         alert(this.username + " : " + passhash);
     };
   });
