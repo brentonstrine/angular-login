8e3133b628daedc06214d4375a6761923f8546f7
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:09:28 -0700

    further renaming

--- index.php
+++ index.php
@@ -7,23 +7,23 @@
   <script src="angular.js"></script>
 </head>
 <body >
-  <div ng-app="login" ng-controller="InvoiceController as invoice">
+  <div ng-app="login1" ng-controller="LoginController as login">
   <b>Invoice:</b>
   <div>
-    Quantity: <input type="number" min="0" ng-model="invoice.qty" required >
+    Quantity: <input type="number" min="0" ng-model="login.qty" required >
   </div>
   <div>
-    Costs: <input type="number" min="0" ng-model="invoice.cost" required >
-    <select ng-model="invoice.inCurr">
-      <option ng-repeat="c in invoice.currencies">{{c}}</option>
+    Costs: <input type="number" min="0" ng-model="login.cost" required >
+    <select ng-model="login.inCurr">
+      <option ng-repeat="c in login.currencies">{{c}}</option>
     </select>
   </div>
   <div>
     <b>Total:</b>
-    <span ng-repeat="c in invoice.currencies">
-      {{invoice.total(c) | currency:c}}
+    <span ng-repeat="c in login.currencies">
+      {{login.total(c) | currency:c}}
     </span>
-    <button class="btn" ng-click="invoice.pay()">Pay</button>
+    <button class="btn" ng-click="login.pay()">Pay</button>
   </div>
 </div>
 </body>
