69e4d8384fe2afe1a9b09356d54cb6a5421cc2a6
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:59:28 -0700

    backend is now authenticating

--- angular.js
+++ angular.js
@@ -2,13 +2,15 @@
   'use strict';
 angular.module('login1', [])
   .controller('LoginController', function($http) {
+    this.username = "default";
+    this.password = "default2";
     this.authenticate = function authenticate() {
         var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
         return this.queryDatabase(this.username, passhash);
     };
     
     this.queryDatabase = function(username, hash) {
-        $http.post('checklogin.php', {username:username, password: hash})
+        $http.post('checklogin.php', {'username':username.toString(), 'hash': hash.toString()})
           .success(function(data, status, headers, config) {
             console.log(data);
           })
--- checklogin.php
+++ checklogin.php
@@ -1,15 +1,14 @@
 <?php
-
-$username = $_POST["username"];
-$hash     = $_POST["hash"];
-
+$params = json_decode(file_get_contents('php://input'),true);
+$username = $params["username"];
+$hash     = $params["hash"];
 $users = array (
     "brenton"  => array("salt" => "nosaltyet", "hash" => "0148600141959ca22640965b5eda51dc"),
     "james"    => array("salt" => "nosaltyet", "hash" => ""),
     "timothy"  => array("salt" => "nosaltyet", "hash" => ""),
 );
-echo $username . " : " . $hash;
 
+// for debugging. echo $username . " : " . $hash;
 if($users[$username]["hash"] == $hash){
     echo "true";
 } else {
