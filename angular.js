b55901fe41ac1c46b831e16d5a455a7c6bfb78e4
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:51:37 -0700

    trying to get backend to receive post

--- angular.js
+++ angular.js
@@ -5,16 +5,14 @@ angular.module('login1', [])
     this.authenticate = function authenticate() {
         var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
         return this.queryDatabase(this.username, passhash);
-        
     };
     
     this.queryDatabase = function(username, hash) {
-        // Simple POST request example (passing data) :
-        $http.post('checklogin.php', {username:username, password: hash}).
-          success(function(data, status, headers, config) {
-            alert(data);
-          }).
-          error(function(data, status, headers, config) {
+        $http.post('checklogin.php', {username:username, password: hash})
+          .success(function(data, status, headers, config) {
+            console.log(data);
+          })
+          .error(function(data, status, headers, config) {
             alert("error " + data);
           });
     };
--- checklogin.php
+++ checklogin.php
@@ -1,5 +1,18 @@
 <?php
 
-echo "true";
+$username = $_POST["username"];
+$hash     = $_POST["hash"];
 
+$users = array (
+    "brenton"  => array("salt" => "nosaltyet", "hash" => "0148600141959ca22640965b5eda51dc"),
+    "james"    => array("salt" => "nosaltyet", "hash" => ""),
+    "timothy"  => array("salt" => "nosaltyet", "hash" => ""),
+);
+echo $username . " : " . $hash;
+
+if($users[$username]["hash"] == $hash){
+    echo "true";
+} else {
+    echo "false";
+}
 ?>
