91d3f862293b2d1b09172d73493f434cad6ad261
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:35:54 -0700

    setting up AJAX request
    currently getting error `ReferenceError: $http is not defined`

--- angular.js
+++ angular.js
@@ -4,20 +4,18 @@ angular.module('login1', [])
   .controller('LoginController', function() {
     this.authenticate = function authenticate() {
         var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
-        alert(this.username + " : " + passhash);
+        return this.queryDatabase(this.username, passhash);
         
     };
     
     this.queryDatabase = function(username, hash) {
         // Simple POST request example (passing data) :
-        $http.post('/someUrl', {msg:'hello word!'}).
+        $http.post('/checklogin.php', {username:username, password: hash}).
           success(function(data, status, headers, config) {
-            // this callback will be called asynchronously
-            // when the response is available
+            alert(data);
           }).
           error(function(data, status, headers, config) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
+            alert("error " + data);
           });
     };
   });
