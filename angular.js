1f2fbefa665f7ddc37816c58d6b831d964df7ac7
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:38:58 -0700

    got ajax working
    needed $http in controller is why

--- angular.js
+++ angular.js
@@ -1,7 +1,7 @@
 (function(angular) {
   'use strict';
 angular.module('login1', [])
-  .controller('LoginController', function() {
+  .controller('LoginController', function($http) {
     this.authenticate = function authenticate() {
         var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
         return this.queryDatabase(this.username, passhash);
@@ -10,7 +10,7 @@ angular.module('login1', [])
     
     this.queryDatabase = function(username, hash) {
         // Simple POST request example (passing data) :
-        $http.post('/checklogin.php', {username:username, password: hash}).
+        $http.post('checklogin.php', {username:username, password: hash}).
           success(function(data, status, headers, config) {
             alert(data);
           }).
