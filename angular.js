f5a42ce4f13ff27c58e9d735372e656cf454ad81
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-06 13:31:19 -0700

    figuring out AJAX
    copy/paste from https://docs.angularjs.org/api/ng/service/$http

--- angular.js
+++ angular.js
@@ -5,6 +5,20 @@ angular.module('login1', [])
     this.authenticate = function authenticate() {
         var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
         alert(this.username + " : " + passhash);
+        
+    };
+    
+    this.queryDatabase = function(username, hash) {
+        // Simple POST request example (passing data) :
+        $http.post('/someUrl', {msg:'hello word!'}).
+          success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+          }).
+          error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
     };
   });
 })(window.angular);
