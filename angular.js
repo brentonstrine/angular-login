38689d25209f55c5b65599e619c8c34a2b158d8f
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2015-06-07 17:01:58 -0700

    changing `this` to `self`

--- angular.js
+++ angular.js
@@ -1,28 +1,26 @@
-(function(angular) {
-  'use strict';
 angular.module('login1', [])
   .controller('LoginController', function($http) {
     
-                console.log(this);
-    this.username = "default";
-    this.password = "default2";
-    this.message = "You are not logged in.";
-    this.authenticate = function authenticate() {
-        var passhash = CryptoJS.MD5(this.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
-        this.queryDatabase(this.username, passhash);
+                var self = this;
+    self.username = "default";
+    self.password = "default2";
+    self.message = "You are not logged in.";
+    self.authenticate = function authenticate() {
+        var passhash = CryptoJS.MD5(self.password); // should be retrieving salt from DB based on username and then adding it to the PW before hashing, but i'll get to that later if there's time.
+        self.queryDatabase(self.username, passhash);
     };
     
-    this.queryDatabase = function(username, hash) {
+    self.queryDatabase = function(username, hash) {
         $http.post('checklogin.php', {'username':username.toString(), 'hash': hash.toString()})
           .success(function(data, status, headers, config) {
             console.log(data);
             if(data=="true"){
                 console.log("successful login");
-                this.message = "Welcome, " + this.username;
+                self.message = "Welcome, " + self.username;
             } else {
                 console.log("failed login");
-                console.log(this);
-                this.message = "That login info is not correct. Please try again.";
+                console.log(self);
+                self.message = "That login info is not correct. Please try again.";
             }
           })
           .error(function(data, status, headers, config) {
@@ -30,4 +28,3 @@ angular.module('login1', [])
           });
     };
   });
-})(window.angular);
